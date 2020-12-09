25d925743ccac537a86e7db26d9b3601d40a608d
carlofabyss <carlofabyss@hotmail.com>
2020-12-08 20:46:24 -0800

    Horizon Graphics Update
    Make it slightly more open to interpretation in most areas

--- route1/sketch.js
+++ route1/sketch.js
@@ -1,54 +1,63 @@
 
 let snowflakes = []; // array to hold snowflake objects
+let snow_flake_color = [];
+let snow_flake_rotate = [];
 
 function setup() {
+  
   createCanvas(windowWidth, windowHeight);
-  fill(240);
   noStroke();
 }
 
 function draw() {
   background("#111");
-  let t = frameCount / 60; // update time
+ 
+  let t = frameCount / 1120; // update time
 
   // create a random number of snowflakes each frame
-  for (let i = 0; i < random(5); i++) {
+  for (let i = 0; i < random(2); i++) {
     snowflakes.push(new snowflake()); // append snowflake object
+    snow_flake_color.push(random(0, 255));
+    // snow_flake_rotate.push(TWO_PI / random(35.0, 40.0));
   }
 
   // loop through snowflakes with a for..of loop
-  for (let flake of snowflakes) {
-    flake.update(t); // update snowflake position
-    flake.display(); // draw snowflake
+  for (i = 0; i < snowflakes.length; i++) {
+    snowflakes[i].update(t); // update snowflake position
+    push();
+    // rotate(snow_flake_rotate[i]);
+    fill(snow_flake_color[i]);
+    snowflakes[i].display(); // draw snowflake
+    pop();
   }
 }
 
 // snowflake class
 function snowflake() {
   // initialize coordinates
-  this.posX = 0;
-  this.posY = random(-50, 0);
-  this.initialangle = random(0, 2 * PI);
-  this.size = random(1, 5);
+  this.posX = random(0, 100);
+  this.posY = 0;
+  this.initialangle = random(0, random(2, 3) * PI);
+  this.size = random(1, 2);
 
   // radius of snowflake spiral
   // chosen so the snowflakes are uniformly spread out in area
-  this.radius = sqrt(random(pow(width / 2, 2)));
+  this.radius = sqrt(random(pow(width / 3, 2)));
 
   this.update = function(time) {
     // x position follows a circle
-    let w = 0.7; // angular speed
+    let w = 0.3; // angular speed
     let angle = w * time + this.initialangle;
     this.posX = width / 2 + this.radius * sin(angle);
 
     // different size snowflakes fall at slightly different y speeds
-    this.posY += pow(this.size, 0.7);
+    this.posY += pow(this.size, 1);
 
     // delete snowflake if past end of screen
-    if (this.posY > height) {
-      let index = snowflakes.indexOf(this);
-      snowflakes.splice(index, 1);
-    }
+    // if (this.posY > height - 50) {
+    //   let index = snowflakes.indexOf(this);
+    //   snowflakes.splice(index, 1);
+    // }
   };
 
   this.display = function() {
--- route2/sketch.js
+++ route2/sketch.js
@@ -1,37 +1,64 @@
 var drop = []
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  for(var i = 0; i < 200; i++) {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  for(var i = 0; i < 1200; i++) {
     drop[i] = new Drop();
-}
+  }
 }
 
 function draw() {
-  background(0);
-  for(var i = 0; i < 200; i++) {
+  background(240);
+
+  push();
+  translate(-windowWidth / 2, 0, 0);
+  for(var i = 0; i < 1200; i++) {
   drop[i].show();
   drop[i].update();
-}
+  drop[i].updatezpoint();
+  }
+  pop();
+
 }
 
 function Drop() {
+
   this.x = random(0, width);
   this.y = random(0, -height);
+  this.z = -3;
   
   this.show = function() {
     noStroke();
-    fill(255);
-    ellipse(this.x, this.y, random(1, 5), random(1, 5));   
+    fill(random(0, 200), fill(random(0, 255)), 0, random(230, 255));
+    ellipse(this.x, this.y, random(1, 12), random(1, 15), 4);   
+
   }
   this.update = function() {
     this.speed = random(5, 20);
-    this.gravity = 1.1;
+    this.gravity = 1.5;
     this.y = this.y + this.speed*this.gravity;  
+    this.z -= 0.5;
     
     if (this.y > height) {
       this.y = random(0, -height);
       this.gravity = 0;
+    }
+
+    if (this.z < -100) {
+      this.z = random(-3, 100);
+    }
+
 }
+
+this.updatezpoint = function() {
+  translate(0, 0, this.z);
 }
+
+
 }
+
+setTimeout(function(){
+  for(var i = 0; i < 1000; i++) {
+    drop.push(new Drop());
+  }
+}, 7000);
