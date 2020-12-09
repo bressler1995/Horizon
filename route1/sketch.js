09166dc3b943a436025d1a8bc3e83ee0e1bbbf4f
carlofabyss <carlofabyss@hotmail.com>
2020-12-09 02:46:04 -0800

    Update P5 animations a little

--- route1/sketch.js
+++ route1/sketch.js
@@ -10,7 +10,7 @@ function setup() {
 }
 
 function draw() {
-  background("#111");
+  background("#000");
  
   let t = frameCount / 1120; // update time
 
@@ -26,10 +26,41 @@ function draw() {
     snowflakes[i].update(t); // update snowflake position
     push();
     // rotate(snow_flake_rotate[i]);
-    fill(snow_flake_color[i]);
+    fill(snow_flake_color[i] - 20, snow_flake_color[i] - 70, snow_flake_color[i] - 70, 255);
     snowflakes[i].display(); // draw snowflake
     pop();
   }
+
+  push();
+  fill(230, 230, 255, 10);
+  ellipse(windowWidth / 2, windowHeight - 70, windowWidth + 120, 135);
+  pop();
+
+  push();
+  fill(230, 230, 255, 10);
+  ellipse(windowWidth / 2, windowHeight - 70, windowWidth + 70, 120);
+  pop();
+
+  push();
+  fill(230, 230, 255, 20);
+  ellipse(windowWidth / 2, windowHeight - 70, windowWidth - 50, 75);
+  pop();
+
+  push();
+  fill(230, 230, 255, 20);
+  ellipse(windowWidth / 2, windowHeight - 70, 450, 45);
+  pop();
+
+  push();
+  fill(230, 230, 255, 10);
+  ellipse(windowWidth / 2, windowHeight - 70, 400, 32);
+  pop();
+
+  push();
+  fill(230, 230, 255, 5);
+  ellipse(windowWidth / 2, windowHeight - 70, 370, 10);
+  pop();
+  
 }
 
 // snowflake class
@@ -37,12 +68,13 @@ function snowflake() {
   // initialize coordinates
   this.posX = random(0, 100);
   this.posY = 0;
-  this.initialangle = random(0, random(2, 3) * PI);
+  this.initialangle = random(0, 2 * PI);
   this.size = random(1, 2);
+  this.raddiv = 7;
 
   // radius of snowflake spiral
   // chosen so the snowflakes are uniformly spread out in area
-  this.radius = sqrt(random(pow(width / 3, 2)));
+  this.radius = sqrt(random(pow(width / 5, 2)));
 
   this.update = function(time) {
     // x position follows a circle
@@ -53,11 +85,11 @@ function snowflake() {
     // different size snowflakes fall at slightly different y speeds
     this.posY += pow(this.size, 1);
 
-    // delete snowflake if past end of screen
-    // if (this.posY > height - 50) {
-    //   let index = snowflakes.indexOf(this);
-    //   snowflakes.splice(index, 1);
-    // }
+    //delete snowflake if past end of screen
+    if (this.posY > height - 80) {
+      let index = snowflakes.indexOf(this);
+      snowflakes.splice(index, 1);
+    }
   };
 
   this.display = function() {
